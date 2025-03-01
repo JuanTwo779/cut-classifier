ca453626513d264c4e57cacb0b3ac1dc2460ca13
juanm <juanmangubat@gmail.com>
2025-03-01 15:51:49 +1100

    Flask REST API

--- app.py
+++ app.py
@@ -26,12 +26,71 @@ data_cat = ['Burst Fade',
 img_height = 180
 img_width = 180
 
-image = tf.keras.utils.load_img(img, target_size=(img_height,img_width))
-img_arr = tf.keras.utils.array_to_img(image)
-img_bat = tf.expand_dims(img_arr,0)
+# image = tf.keras.utils.load_img(img, target_size=(img_height,img_width))
+# img_arr = tf.keras.utils.array_to_img(image)
+# img_bat = tf.expand_dims(img_arr,0)
+# predict = model.predict(img_bat)
+# score = tf.nn.softmax(predict)
+# print('Haircut in image is a {} with accuracy of {:0.2f}'.format(data_cat[np.argmax(score)],np.max(score)*100))
 
-predict = model.predict(img_bat)
+from flask import Flask, request, jsonify
+from io import BytesIO
 
-score = tf.nn.softmax(predict)
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 
-print('Haircut in image is {} with accuracy of {:0.2f}'.format(data_cat[np.argmax(score)],np.max(score)*100))
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/test")
+def model_test():
+    image = tf.keras.utils.load_img(img, target_size=(img_height,img_width))
+    img_arr = tf.keras.utils.array_to_img(image)
+    img_bat = tf.expand_dims(img_arr,0)
+
+    predict = model.predict(img_bat)
+
+    score = tf.nn.softmax(predict)
+
+    return 'Haircut in image is a {} with an accuracy of {:0.2f}'.format(data_cat[np.argmax(score)],np.max(score)*100)
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+def preprocess_image(file):
+    image = tf.keras.utils.load_img(BytesIO(file.read()), target_size=(img_height,img_width))
+    img_arr = tf.keras.utils.array_to_img(image)
+    img_bat = tf.expand_dims(img_arr,0)
+    return img_bat
+
+@app.post("/predict")
+def predict_cut():
+
+    # 1. take in JPG 
+    if "file" not in request.files:
+        return jsonify({"error":"No file upload"}), 400
+    
+    file = request.files['file']
+
+    # 2. validate image
+    if file.filename == "":  # Handle empty filename
+        return jsonify({"error": "No selected file"}), 400
+    
+    if file and not allowed_file(file.filename):
+        return jsonify({"error":"file type not allowed"}), 400
+
+    # 3. fix image
+    img_bat = preprocess_image(file)
+
+    # 4. make prediction and produce score
+    predict = model.predict(img_bat)
+    score = tf.nn.softmax(predict)
+    
+    # 5. return category and score to frontend
+    return jsonify({"success": 'Haircut in image is a {} with an accuracy of {:0.2f}'.format(data_cat[np.argmax(score)],np.max(score)*100) })
+
+
+
